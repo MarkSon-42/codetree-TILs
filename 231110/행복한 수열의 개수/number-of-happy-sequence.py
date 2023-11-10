dbc63ba371ec88ade6b79a261ed8ada51ef48d15
Minwoo Son <mark42son@gmail.com>
2023-11-10 09:47:25 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231110/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231110/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,33 +1,43 @@
-def count_happy_sequences(n, m, grid):
-    def count_consecutive(sequence):
-        count = 1
-        max_count = 1
-        for i in range(1, len(sequence)):
-            if sequence[i] == sequence[i - 1]:
-                count += 1
-                max_count = max(max_count, count)
-            else:
-                count = 1
-        return max_count
-
-    happy_sequences = 0
-
-    # Check rows
-    for row in grid:
-        if count_consecutive(row) >= m:
-            happy_sequences += 1
-
-    # Check columns
-    for j in range(n):
-        column = [grid[i][j] for i in range(n)]
-        if count_consecutive(column) >= m:
-            happy_sequences += 1
-
-    print(happy_sequences)
-
-# Read input
 n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Count and print happy sequences
-count_happy_sequences(n, m, grid)
+grid = [list(map(int, input().split()))]
+
+seq = [0 for _ in range(n)]
+
+def is_happy_seq():
+    consec_count, max_count = 1, 1
+    for i in range(1, n):
+        if seq[i - 1] == seq[i]:
+            consec_count += 1  # 연속 카운트
+        else:
+            consec_count = 1  # 연속이 깨지면 1로 갱신
+
+        max_count = max(consec_count, max_count)
+
+    # 최대로 연속한 회수가 m이상이면 true반환
+    return max_count >= m
+
+
+
+num_happy = 0
+
+
+# 가로로 행복한 수열의 수 세기
+for i in range(n):
+    seq = grid[i][:]
+
+    if is_happy_seq():
+        num_happy += 1
+
+# 세로
+
+for j in range(n):
+    #  ! : 세로로 숫자들을 모아 새로운 수열을 만들기  -> 세로 요소를 가로로 재배치해서 수열 행태로
+    # 그래야 is_happy_seq()의 파라미터에 맞출 수 있음!
+    for i in range(n):
+        seq[i] = grid[i][j]
+
+    if is_happy_seq():
+        num_happy += 1
+
+print(num_happy)
