653eadbdd3110068108e119f6b2408f15240247d
Minwoo Son <mark42son@gmail.com>
2023-11-20 12:53:07 +0900

    Title: 사각형 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231120/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define MAX_N 1000
+
+using namespace std;
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    int dp[MAX_N + 1] = {};
+    dp[1] = 2;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n];
+    return 0;
+}
