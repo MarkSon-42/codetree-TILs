21e28c69cd11899ccda9db6db550fb9d12a9127f
Minwoo Son <mark42son@gmail.com>
2023-12-06 05:30:47 +0900

    Title: 배열 놀이, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/배열 놀이/play-with-array.py
+++ 231206/배열 놀이/play-with-array.py
@@ -2,16 +2,17 @@ n, q = map(int, input().split())
 
 elems = [0] + list(map(int, input().split()))
 
-for i in range(q):
+for _ in range(q):
     query = list(map(int, input().split()))
     if query[0] == 1:
-        print(query[1])
-    elif query[0] == 2:
+        print(elems[query[1]])
+    if query[0] == 2:
         if query[1] in elems:
             print(elems.index(query[1]))
         else:
             print(0)
-    elif query[0] == 3:
+    if query[0] == 3:
         a, b = query[1], query[2]
         for j in range(a, b + 1):
             print(elems[j], end = ' ')
+        print()
