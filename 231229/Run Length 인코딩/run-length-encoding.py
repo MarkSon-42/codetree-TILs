2ba1d1b4bc319b94c018896aae376ba42490cbab
Minwoo Son <mark42son@gmail.com>
2023-12-29 17:16:05 +0900

    Title: Run Length 인코딩, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231229/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,19 @@
+cnt = 1
+answer = []
+string = input()
+
+for i in range(len(string) - 1):
+    if string[i] == string[i + 1]:
+        cnt += 1
+        if i == len(string) - 2:
+            answer.append(string[i])
+            answer.append(str(cnt))
+            break
+        continue
+    else:
+        answer.append(string[i])
+        answer.append(str(cnt))
+        cnt = 1
+
+print(len(answer))
+print(''.join(answer))
