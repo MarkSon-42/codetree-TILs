9e9ce3b4aa5392069f636cfa213bc198c991e68f
Minwoo Son <mark42son@gmail.com>
2023-12-29 17:34:24 +0900

    Title: Run Length 인코딩, Time: 141ms, Memory: 73MB, Status: Passed - CodeTree

--- 231229/Run Length 인코딩/run-length-encoding.py
+++ 231229/Run Length 인코딩/run-length-encoding.py
@@ -1,26 +1,22 @@
-cnt = 1
-answer = []
-string = input()
+def run_length_encoding(s):
+    encoded_result = ""
+    count = 1
 
-for i in range(len(string) - 1):
-    if i == len(string) - 1 and string[i + 1] != string[i]:
-        answer.append(string[i + 1])
-        answer.append('1')
-    if string[i] == string[i + 1]:
-        cnt += 1
-        if i == len(string) - 2:
-            answer.append(string[i])
-            answer.append(str(cnt))
-            break
-        continue
-    else:
-        # if i == len(string) - 1:
-        #     answer.append(string[i])
-        #     answer.append("1")
-        #     break
-        answer.append(string[i])
-        answer.append(str(cnt))
-        cnt = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            encoded_result += s[i - 1] + str(count)
+            count = 1
 
-print(len(answer))
-print("".join(answer))
+    encoded_result += s[-1] + str(count)  # Adding the last character and its count
+
+    return len(encoded_result), encoded_result
+
+if __name__ == "__main__":
+    A = input().strip()
+
+    length, result = run_length_encoding(A)
+
+    print(length)
+    print(result)
