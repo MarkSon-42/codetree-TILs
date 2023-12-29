f4876bf3b9f54e3343311343fb0722d6313c61ee
Minwoo Son <mark42son@gmail.com>
2023-12-29 17:50:11 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231229/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 231229/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -1,6 +1,11 @@
-string = input()
-reversed_string = string[::-1]
+# Function to reverse even-numbered characters of a string
+def reverse_even_characters(input_string):
+    even_characters = input_string[1::2]  # Extract even-numbered characters
+    reversed_even_characters = even_characters[::-1]  # Reverse the even-numbered characters
+    print(reversed_even_characters)
 
-for i in range(len(reversed_string)):
-    if i % 2 == 0:
-        print(reversed_string[i], end='')
+# Input
+input_string = input().strip()
+
+# Output
+reverse_even_characters(input_string)
