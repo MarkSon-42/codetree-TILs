113baa284dba2b6f30f405c91b7638d70fa6576b
Minwoo Son <mark42son@gmail.com>
2024-05-10 21:24:50 +0900

    Title: 1차원 윷놀이, Time: 455ms, Memory: 29MB, Status: Passed - CodeTree

--- 240510/1차원 윷놀이/yutnori-1d.py
+++ 240510/1차원 윷놀이/yutnori-1d.py
@@ -3,33 +3,31 @@ se = sys.stdin.readline
 
 n, m, k = map(int, se().split())
 scores = list(map(int, se().split()))
-yuts = []
-positions = []
+yuts = [1 for _ in range(k)]
 
 answer = 0
 
 def calc():
     global answer
-    positions = [1 for i in range(len(yuts))]
-    # 최초로 m을 넘었는지 판별하는 리스트
-    overed = [False for i in range(len(yuts))]
     temp = 0
-    for idx, i in enumerate(yuts):
-        positions[i-1] += scores[idx]
-        if positions[i-1] >= m and not overed[i-1]:
-            overed[i-1] = True
+    for yut in yuts:
+        if yut >= m:
             temp += 1
-    answer = max(answer, temp)
+    return temp
 
 def recursion(cnt):
+    global answer
+    answer = max(answer, calc())
+
     if cnt == n:
-        calc()
         return
 
     for i in range(k):
-        yuts.append(i)
+        if yuts[i] >= m:
+            continue
+        yuts[i] += scores[cnt]
         recursion(cnt+1)
-        yuts.pop()
+        yuts[i] -= scores[cnt]
 
 
 recursion(0)
