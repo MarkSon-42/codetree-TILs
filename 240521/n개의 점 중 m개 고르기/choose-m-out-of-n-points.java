39b012124a877b371a51bf430f012a90fbece343
Minwoo Son <mark42son@gmail.com>
2024-05-21 16:01:33 +0900

    Title: n개의 점 중 m개 고르기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240521/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 240521/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -13,42 +13,23 @@ class Pair {
 public class Main {
     public static int n, m;
     public static int answer = Integer.MAX_VALUE;
+    public static int maxDist = Integer.MAX_VALUE;
     public static List<Pair> pairList = new ArrayList<>();
     public static List<Pair> selectList = new ArrayList<>();
+    // 고른 애들 중에서 가장 먼 점 찾을때 쓰는 리스트
+    public static List<Pair> tempList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException{
         input();
         find(0, 0);
-        System.out.print((int) (answer));
-    }
-
-    public static int calc(){
-        int dist = Integer.MAX_VALUE;
-
-        for(int i = 0; i < selectList.size()-1; i++){
-            Pair tempA = selectList.get(i);
-            int ax = tempA.x;
-            int ay = tempA.y;
-            for(int j = i+1; j < selectList.size(); j++){
-                Pair tempB = selectList.get(j);
-                int bx = tempB.x;
-                int by = tempB.y;
-
-                int tempDist = (int) Math.pow(ax-bx, 2) + (int) Math.pow(ay-by, 2);
-
-                // System.out.println("현재 A = " + ax + ", " + ay);
-                // System.out.println("현재 B = " + bx + ", " + by);
-                if(tempDist < dist)
-                    dist = tempDist;
-            }
-        }
-        return dist;
+        System.out.print(answer);
     }
 
     public static void find(int currIdx, int cnt){
         if(currIdx == n){
             if(cnt == m){
-                answer = Math.min(answer, calc());
+                findPairs(0, 0);
+                answer = Math.min(answer, maxDist);
             }
             return;
         }
@@ -60,6 +41,30 @@ public class Main {
 
         // 현재 좌표를 고르지 않은 경우
         find(currIdx+1, cnt);
+    }
+
+    public static int getDist(int ax, int ay, int bx, int by){
+        // System.out.println("현재 A = " + ax + ", " + ay);
+        // System.out.println("현재 B = " + bx + ", " + by);
+        // System.out.println("최대거리 = " + ((int) Math.pow(ax-bx, 2) + (int) Math.pow(ay-by, 2)));
+        return (int) Math.pow(ax-bx, 2) + (int) Math.pow(ay-by, 2);
+    }
+
+    public static void findPairs(int currIdx, int cnt){
+        if(currIdx == m){
+            if(cnt == 2){
+                Pair a = tempList.get(0);
+                Pair b = tempList.get(1);
+                maxDist = Math.min(maxDist, getDist(a.x, a.y, b.x, b.y));
+            }
+            return;
+        }
+
+        tempList.add(selectList.get(currIdx));
+        findPairs(currIdx+1, cnt+1);
+        tempList.remove(tempList.size()-1);
+
+        findPairs(currIdx+1, cnt);
     }
 
     public static void input() throws IOException {
