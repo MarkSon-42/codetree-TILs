77469fab2cf42444ab0626aa039657380eccee2c
Minwoo Son <mark42son@gmail.com>
2024-05-21 15:37:32 +0900

    Title: n개의 점 중 m개 고르기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240521/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 240521/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -12,18 +12,18 @@ class Pair {
 
 public class Main {
     public static int n, m;
-    public static int answer = Integer.MAX_VALUE;
+    public static double answer = Double.MAX_VALUE;
     public static List<Pair> pairList = new ArrayList<>();
     public static List<Pair> selectList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException{
         input();
         find(0, 0);
-        System.out.print(answer * answer);
+        System.out.print((int) (answer * answer));
     }
 
-    public static int calc(){
-        int dist = 0;
+    public static double calc(){
+        double dist = Double.MAX_VALUE;
 
         for(int i = 0; i < selectList.size()-1; i++){
             Pair tempA = selectList.get(i);
@@ -34,15 +34,14 @@ public class Main {
                 int bx = tempB.x;
                 int by = tempB.y;
 
-                int tempDist = (int) Math.sqrt((int) Math.pow(ax-bx, 2) + (int) Math.pow(ay-by, 2));
+                double tempDist = Math.sqrt((int) Math.pow(ax-bx, 2) + (int) Math.pow(ay-by, 2));
 
-                // System.out.println("현재 i = " + i);
-                // System.out.println("현재 j = " + j);
-                if(tempDist > dist)
+                // System.out.println("현재 A = " + ax + ", " + ay);
+                // System.out.println("현재 B = " + bx + ", " + by);
+                if(tempDist < dist)
                     dist = tempDist;
             }
         }
-
         return dist;
     }
 
