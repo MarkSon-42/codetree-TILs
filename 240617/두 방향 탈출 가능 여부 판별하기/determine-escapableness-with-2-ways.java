206c0d96bc37bc86a7f568a7db1c282839f78b40
Minwoo Son <mark42son@gmail.com>
2024-06-17 13:27:02 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 248ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240617/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240617/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -5,12 +5,13 @@ public class Main {
     public static int n, m;
     public static int[][] grid;
     public static int[][] visited;
+    public static int answer = 0;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         input();
-        int answer = dfs(0, 0);
+        dfs(0, 0);
         System.out.print(answer);
     }
 
@@ -18,10 +19,10 @@ public class Main {
         return r >= 0 && r < n && c >= 0 && c < n;
     }
 
-    public static int dfs(int r, int c) {
+    public static void dfs(int r, int c) {
         visited[r][c] = 1;
         if(r == n && c == m)
-            return 1;
+            answer = 1;
         
         for(int i = 0; i < 4; i++){
             int nr = r + dr[i];
@@ -33,8 +34,6 @@ public class Main {
 
             dfs(nr, nc);
         }
-
-        return 0;
     }
 
     public static void input() throws IOException {
