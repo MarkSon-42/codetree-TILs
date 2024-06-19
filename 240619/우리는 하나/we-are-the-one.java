11f6629eda8b06ff20237ddeb3f0e961a19c1a4c
Minwoo Son <mark42son@gmail.com>
2024-06-19 18:32:23 +0900

    Title: 우리는 하나, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240619/우리는 하나/we-are-the-one.java
@@ -0,0 +1,110 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, k, u, d;
+
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static List<int[]> cities = new ArrayList<>();
+    public static List<int[]> selectedCities = new ArrayList<>();
+
+    public static int[] dr = new int[]{-1, 1, 0, 0};
+    public static int[] dc = new int[]{0, 0, -1, 1};
+
+    public static int answer = 0;
+
+    public static void main(String[] args) throws IOException{
+        input();
+        findMax(0, 0);
+        System.out.print(answer+1);
+    }
+
+    public static boolean inRange(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < n;
+    }
+
+    public static boolean heightPossible(int r, int c, int nr, int nc) {
+        int gap = Math.abs(grid[r][c] - grid[nr][nc]);
+        return gap >= u && gap <= d ? true : false;
+    }
+
+    public static boolean canGo(int r, int c, int nr, int nc) {
+        if(!inRange(nr, nc) || !heightPossible(r, c, nr, nc) || visited[nr][nc])
+            return false;
+        return true;
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        u = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][n];
+        visited = new boolean[n][n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+                cities.add(new int[]{i, j});
+            }
+        }
+    }
+
+    public static void bfs(int[] city) {
+        Deque<int[]> q = new ArrayDeque<>();
+        int r = city[0];
+        int c = city[1];
+
+        visited[r][c] = true;
+
+        for(int i = 0; i < 4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(canGo(r, c, nr, nc)){
+                visited[nr][nc] = true;
+                r = nr;
+                c = nc;
+                q.add(new int[]{nr, nc});
+            }
+        }
+    }
+
+    public static int calc() {
+        visited = new boolean[n][n];
+        int cnt = 0;
+
+        for(int[] city : selectedCities) {
+            bfs(city);
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(visited[i][j]){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void findMax(int idx, int cnt) {
+        if(idx == cities.size()) {
+            if(cnt == k) {
+                answer = Math.max(answer, calc());
+            }
+            return;
+        }
+
+        selectedCities.add(cities.get(idx));
+        findMax(idx+1, cnt+1);
+        selectedCities.remove(selectedCities.size()-1);
+        findMax(idx+1, cnt);
+    }
+}
