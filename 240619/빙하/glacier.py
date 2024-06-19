8b5a8b002a15cd5002e2da4ba6023bf3604a2e29
Minwoo Son <mark42son@gmail.com>
2024-06-19 17:25:17 +0900

    Title: 빙하, Time: 1482ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240619/빙하/glacier.py
@@ -0,0 +1,71 @@
+ICE = 1
+WATER = 0
+import sys
+input = sys.stdin.readline 
+from collections import deque
+
+
+dxs, dys = [0,0,1,-1], [1,-1,0,0]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+time, last_ice_size = 0, 0
+que = deque()
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def get_ice_size():
+    global arr
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == ICE:
+                result += 1
+    return result
+
+def get_outer_water():
+    visited = [[False]*m for _ in range(n)]
+    que.append((0,0))
+    visited[0][0] = True
+    outer_water_lst = [(0,0)]
+
+    while que:
+        x, y = que.popleft()
+        for k in range(4):
+            nx, ny = x+dxs[k], y+dys[k]
+            if not in_range(nx, ny):
+                continue
+            if not visited[nx][ny]:
+                if arr[nx][ny] == WATER:
+                    que.append((nx, ny))
+                    visited[nx][ny] = True
+                    outer_water_lst.append((nx, ny))
+    return outer_water_lst
+
+def melt_ice(outer_water):
+    global arr
+
+    for outer_water_pos in outer_water_lst:
+        x, y = outer_water_pos
+        for k in range(4):
+            nx, ny = x+dxs[k], y+dys[k]
+            if not in_range(nx, ny):
+                continue
+            if arr[nx][ny] == ICE:
+                arr[nx][ny] = WATER
+    return   
+
+def ice_exist():
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == ICE:
+                return True
+    return False
+    
+while ice_exist():
+    time += 1
+    outer_water_lst = get_outer_water()
+    last_ice_size = get_ice_size()
+    melt_ice(outer_water_lst)
+
+print(time, last_ice_size)
