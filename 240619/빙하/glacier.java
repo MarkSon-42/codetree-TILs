6f6e9b91ed01842fd6bc73f84ccb29d0e25bad45
Minwoo Son <mark42son@gmail.com>
2024-06-19 17:27:22 +0900

    Title: 빙하, Time: 1123ms, Memory: 51MB, Status: Passed - CodeTree

--- 240619/빙하/glacier.java
+++ 240619/빙하/glacier.java
@@ -94,7 +94,7 @@ public class Main {
     }
 
     public static boolean canGo(int r, int c) {
-        if(!inRange(r, c) || grid[r][c] == 1)
+        if(!inRange(r, c) || grid[r][c] == 1 || visited[r][c])
             return false;
         return true;
     }
