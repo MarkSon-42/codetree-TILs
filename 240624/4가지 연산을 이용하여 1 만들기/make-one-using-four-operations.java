42f2b88cced24aa9466d8af879ef4b33a0a5ccd2
Minwoo Son <mark42son@gmail.com>
2024-06-24 16:03:11 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240624/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int[] visited = new int[1000001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{n, 0});
+        visited[0] = 1;
+
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            int num = temp[0];
+            int cnt = temp[1];
+
+            if(num == 1){
+                System.out.print(cnt);
+                break;
+            }
+
+            if(visited[num-1] == 0) {
+                q.add(new int[]{num-1, cnt+1});
+                visited[num-1] = visited[num] + 1;
+            }
+            if(visited[num+1] == 0) {
+                q.add(new int[]{num+1, cnt+1});
+                visited[num+1] = visited[num] + 1;
+            }
+
+            if(num % 2 == 0 && visited[num/2] == 0) {
+                q.add(new int[]{num/2, cnt+1});
+                visited[num/2] = visited[num] + 1;
+            }
+            if(num % 3 == 0 && visited[num/3] == 0) {
+                q.add(new int[]{num/3, cnt+1});
+                visited[num/3] = visited[num] + 1;
+            }
+        }
+    }
+}
