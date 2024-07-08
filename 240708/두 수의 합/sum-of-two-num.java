026597410add81fb0e311e7568a9bbed694b5eaf
Minwoo Son <mark42son@gmail.com>
2024-07-08 17:14:09 +0900

    Title: 두 수의 합, Time: 618ms, Memory: 30MB, Status: Passed - CodeTree

--- 240708/두 수의 합/sum-of-two-num.java
+++ 240708/두 수의 합/sum-of-two-num.java
@@ -3,32 +3,32 @@ import java.io.*;
 
 public class Main {
     public static int n, k;
-    public static int[] arr;
+    public static long[] arr;
     public static int answer = 0;
     public static List<Integer> selectedNums = new ArrayList<>();
 
-    public static Map<String, String> map = new HashMap<>();
+    public static Map<Long, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException{
         input();
-        findCombi(0, 0);
+        find();
         System.out.print(answer);
     }
 
-    public static void findCombi(int idx, int cnt) {
-        if(idx == n){
-            if(cnt == 2){
-                if(selectedNums.stream().mapToInt(Integer::intValue).sum() == k){
-                    answer++;
-                }
-            }
-            return;
-        }
+    public static void find() {
+        for(int i = 0; i < n; i++){
+            long diff = k - arr[i];
 
-        selectedNums.add(arr[idx]);
-        findCombi(idx + 1, cnt + 1);
-        selectedNums.remove(selectedNums.size() - 1);
-        findCombi(idx + 1, cnt);
+            if(map.containsKey(diff)){
+                answer += map.get(diff);
+            }
+
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+        }
     }
 
     public static void input() throws IOException{
@@ -38,7 +38,7 @@ public class Main {
         k = Integer.parseInt(st.nextToken());
 
         arr = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
+                    .mapToLong(Long::parseLong)
                     .toArray();
     }
 }
