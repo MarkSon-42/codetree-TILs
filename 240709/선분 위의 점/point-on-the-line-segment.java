25eed9b7b4cbdd726808bd6c7fb607aabc8ceea0
Minwoo Son <mark42son@gmail.com>
2024-07-09 17:22:07 +0900

    Title: 선분 위의 점, Time: 237ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240709/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] dotArr;
+    public static int[][] lineArr;
+    public static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+
+    public static int find(int start, int end, int target) {
+        int l = start;
+        int r = end-1;
+
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(mid == target){
+                return 1;
+            }
+            if(target < mid)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return 0;
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        dotArr = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+        lineArr = new int[m][2];
+
+        for(int i = 0; i < m; i++){
+            lineArr[i] = Arrays.stream(br.readLine().split(" "))
+                                .mapToInt(Integer::parseInt)
+                                .toArray();
+            
+            int start = lineArr[i][0];
+            int end = lineArr[i][1];
+
+            int cnt = 0;
+
+            for(int target : dotArr){
+                cnt += find(start, end, target);
+            }
+            System.out.println(cnt);
+        }
+    }
+}
