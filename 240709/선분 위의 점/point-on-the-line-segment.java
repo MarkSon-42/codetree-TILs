5972ac3fbd7fceb8acb560bd7dc7700f185c8001
Minwoo Son <mark42son@gmail.com>
2024-07-09 17:57:21 +0900

    Title: 선분 위의 점, Time: 522ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 240709/선분 위의 점/point-on-the-line-segment.java
+++ 240709/선분 위의 점/point-on-the-line-segment.java
@@ -16,10 +16,10 @@ public class Main {
 
         while(l <= r) {
             int mid = (l + r) / 2;
-            if(dotArr[mid] >= start)
-                r = mid - 1;
-            else
+            if(dotArr[mid] < start)
                 l = mid + 1;
+            else
+                r = mid - 1;
         }
         return r + 1;
     }
@@ -30,7 +30,7 @@ public class Main {
 
         while(l <= r) {
             int mid = (l + r) / 2;
-            if(dotArr[mid] > end)
+            if(end < dotArr[mid])
                 r = mid - 1;
             else
                 l = mid + 1;
